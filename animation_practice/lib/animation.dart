083af0acc3f6c985388f879012ca6eaf160ae6de
Caro <carolinegyireh@gmail.com>
2024-07-19 07:31:04 +0200

    updtes

---
+++ animation_practice/lib/animation.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+/// Flutter code sample for [Hero].
+
+void main() {
+  // Slow down time to see Hero flight animation.
+  timeDilation = 15.0;
+  runApp(const AnimationApp());
+}
+
+class AnimationApp extends StatelessWidget {
+  const AnimationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animation Practice',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AnimationExample(),
+    );
+  }
+}
+
+class HeroExample extends StatelessWidget {
+  const HeroExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Hero(
+              tag: 'hero-default-tween',
+              child: BoxWidget(
+                size: const Size(50.0, 50.0),
+                color: Colors.limeAccent[700]!.withOpacity(0.5),
+              ),
+            ),
+            title: const Text(
+              'This red icon will use a default rect tween during the hero flight.',
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          ListTile(
+            leading: Hero(
+              tag: 'hero-custom-tween',
+              createRectTween: (Rect? begin, Rect? end) {
+                return MaterialRectCenterArcTween(begin: begin, end: end);
+              },
+              child: BoxWidget(
+                size: const Size(50.0, 50.0),
+                color: Colors.pink[700]!.withOpacity(0.5),
+              ),
+            ),
+            title: const Text(
+              'This blue icon will use a custom rect tween during the hero flight.',
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => _gotoDetailsPage(context),
+            child: const Text('Tap me'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _gotoDetailsPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Second Page'),
+        ),
+        body: Align(
+          alignment: Alignment.bottomRight,
+          child: Stack(
+            children: <Widget>[
+              Hero(
+                tag: 'hero-custom-tween',
+                createRectTween: (Rect? begin, Rect? end) {
+                  return MaterialRectCenterArcTween(begin: begin, end: end);
+                },
+                child: BoxWidget(
+                  size: const Size(400.0, 400.0),
+                  color: Colors.limeAccent[700]!.withOpacity(0.5),
+                ),
+              ),
+              Hero(
+                tag: 'hero-default-tween',
+                child: BoxWidget(
+                  size: const Size(400.0, 400.0),
+                  color: Colors.pink[700]!.withOpacity(0.5),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class BoxWidget extends StatelessWidget {
+  const BoxWidget({
+    super.key,
+    required this.size,
+    required this.color,
+  });
+
+  final Size size;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: color,
+    );
+  }
+}
